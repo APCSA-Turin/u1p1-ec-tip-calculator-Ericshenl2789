f8f86971731134f64f37f7bc7f86e9e6d91fd4f2
Ericshenl2789 <ericshenl2789@nycstudents.net>
2024-09-20 13:25:50 +0000

    second commit

--- src/main/java/com/example/project/ExtraCredit.java
+++ src/main/java/com/example/project/ExtraCredit.java
@@ -7,10 +7,23 @@ public class ExtraCredit {
 
     public static String calculateTip(int people, int percent, double cost, String items) {
         StringBuilder result = new StringBuilder();
+        
+        double tip = Math.round(cost * percent + 0.5) / 100.0; //learned from https://stackoverflow.com/questions/11701399/round-up-to-2-decimal-places-in-java
+        double totalBill = cost + tip;
+        double perPersonCost = Math.round(cost / people * 100 + 0.05) / 100.0;
+        double tipPerPerson = Math.round(tip / people * 100 + 0.05) / 100.0;
+        double totalCostPerPerson = (int)(totalBill / people * 100) / 100.0;
 
-                //COPY AND PASTE YOUR CODE HERE 
-
-        //the two lines  should go below result.append("-------------------------------\n"); 
+        
+        result.append("-------------------------------\n");
+        result.append("Total bill before tip: " + "$" + cost + "\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total percentage: " + percent + "%\n");
+        result.append("Total tip: " + "$" + tip + "\n");
+        result.append("Total Bill with tip: " + "$" + totalBill + "\n");
+        result.append("Per person cost before tip: " + "$" + perPersonCost + "\n");
+        result.append("Tip per person: " + "$" + tipPerPerson + "\n");
+        result.append("Total cost per person: " + "$" + totalCostPerPerson + "\n");
+        result.append("-------------------------------\n");
         result.append("Items ordered:\n");
         result.append(items);
 
@@ -19,13 +32,23 @@ public class ExtraCredit {
     }
                                    
     public static void main(String[] args) {
-        int people;
-        int percent;
-        double cost;
-        String items; 
+        int people = 6;
+        int percent = 25;
+        double cost = 52.27;
+        String items = "";
+        String userInput = "";
 
         //Your scanner object and while loop should go here
-                             
+        Scanner scan = new Scanner(System.in);
+
+        while (!userInput.equals("-1")){
+            System.out.println("Enter an item name or type -1 to finish: ");
+            userInput = scan.nextLine();
+            if(!userInput.equals("-1")){
+                items = items + userInput + "\n";
+            }
+        }
+
         System.out.println(calculateTip(people,percent,cost,items));
     }
 }
